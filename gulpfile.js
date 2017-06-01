0d64c847e2c7c01eb70c6d35dc4fab0e3f127681
Ármin Scipiades <scipiades.armin@gmail.com>
2017-06-02 00:09:54 +0200

    Add tslint, better watchify

--- gulpfile.js
+++ gulpfile.js
@@ -1,36 +1,75 @@
 var gulp = require("gulp");
 var browserify = require("browserify");
 var source = require('vinyl-source-stream');
+var merge = require('merge-stream');
 var watchify = require("watchify");
 var tsify = require("tsify");
 var gutil = require("gulp-util");
 var cachebust = require("gulp-cache-bust");
+var tslint = require('gulp-tslint');
 
 var paths = {
-    pages: ['src/*.html']
+    pages: ['src/*.html'],
+    typeScripts: ['src/**/*.ts']
 };
 
-var watchedBrowserify = watchify(browserify({
+var browserifyConfig = {
     basedir: '.',
     debug: true,
     entries: ['src/main.ts'],
     cache: {},
     packageCache: {}
-}).plugin(tsify));
+};
+
+gulp.task("lint", function() {
+    return gulp.src(paths.typeScripts)
+        .pipe(tslint({
+            formatter: "stylish"
+        }))
+        .pipe(tslint.report());
+});
 
 gulp.task("copy-html", function () {
     return gulp.src(paths.pages)
-    	.pipe(cachebust({type: 'timestamp'}))
+        .pipe(cachebust({type: 'timestamp'}))
         .pipe(gulp.dest("dist"));
 });
 
-function bundle() {
-    return watchedBrowserify
+gulp.task("bundle", ["lint"], function() {
+    return browserify(browserifyConfig)
+        .plugin(tsify)
         .bundle()
         .pipe(source('bundle.js'))
         .pipe(gulp.dest("dist"));
-}
+});
 
-gulp.task("default", ["copy-html"], bundle);
-watchedBrowserify.on("update", bundle);
-watchedBrowserify.on("log", gutil.log);
+gulp.task("default", ["copy-html", "bundle"]);
+
+gulp.task("watch", function() {
+    var watch = watchify(browserify(browserifyConfig).plugin(tsify));
+
+    var update = function(changedFiles) {
+        var compileStream = watch
+            .bundle()
+            .on('error', gutil.log.bind(gutil, gutil.colors.red('browserify error\n')))
+            .pipe(source('bundle.js'))
+            .pipe(gulp.dest("dist"));
+
+        if(!changedFiles) return compileStream;
+
+        var lintStream = gulp.src(changedFiles)
+            .pipe(tslint({
+                formatter: "stylish"
+            }))
+            .pipe(tslint.report({
+                emitError: false
+            }));
+
+        return merge(lintStream, compileStream);
+    };
+
+    watch.on("update", update);
+    watch.on("log", gutil.log);
+
+    return update();
+});
