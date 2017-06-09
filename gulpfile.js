39301128343d81e24e519001d37de06598448a01
Ármin Scipiades <scipiades.armin@gmail.com>
2017-06-09 22:27:09 +0200

    Use ES2015, babel, uglify

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,15 @@
 var gulp = require("gulp");
 var browserify = require("browserify");
 var source = require('vinyl-source-stream');
+var buffer = require('vinyl-buffer');
 var merge = require('merge-stream');
 var watchify = require("watchify");
 var tsify = require("tsify");
+var sourcemaps = require('gulp-sourcemaps');
 var gutil = require("gulp-util");
 var cachebust = require("gulp-cache-bust");
 var tslint = require('gulp-tslint');
+var uglify = require('gulp-uglify');
 
 var paths = {
     pages: ['src/*.html'],
@@ -38,21 +41,38 @@ gulp.task("copy-html", function () {
 gulp.task("bundle", ["lint"], function() {
     return browserify(browserifyConfig)
         .plugin(tsify)
+        .transform('babelify', {
+            presets: ['es2015'],
+            extensions: ['.ts']
+        })
         .bundle()
         .pipe(source('bundle.js'))
+        .pipe(buffer())
+        .pipe(sourcemaps.init({ loadMaps: true }))
+        .pipe(uglify())
+        .pipe(sourcemaps.write('./'))
         .pipe(gulp.dest("dist"));
 });
 
 gulp.task("default", ["copy-html", "bundle"]);
 
 gulp.task("watch", function() {
-    var watch = watchify(browserify(browserifyConfig).plugin(tsify));
+    var watch = watchify(browserify(browserifyConfig)
+        .plugin(tsify))
+        .transform('babelify', {
+            presets: ['es2015'],
+            extensions: ['.ts']
+        });
 
     var update = function(changedFiles) {
         var compileStream = watch
             .bundle()
             .on('error', gutil.log.bind(gutil, gutil.colors.red('browserify error\n')))
             .pipe(source('bundle.js'))
+            .pipe(buffer())
+            .pipe(sourcemaps.init({ loadMaps: true }))
+            .pipe(uglify())
+            .pipe(sourcemaps.write('./'))
             .pipe(gulp.dest("dist"));
 
         if(!changedFiles) return compileStream;
